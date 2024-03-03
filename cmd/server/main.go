c132bfb1af2fae88d6e8794a772b44788a6d2e21
Ruslan Golovizin <r.golovizin@webinar.ru>
2024-03-03 17:28:43 +0300

    Простой сервер с хэндлером доступный на localhost:8080

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import "net/http"
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", defaultHandler)
+
+	http.ListenAndServe("localhost:8080", mux)
+}
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("test"))
+}
