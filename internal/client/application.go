da9410cef07bdc14dd1684041111fb78c98226b5
Ruslan Golovizin <r.golovizin@webinar.ru>
2024-03-11 22:48:45 +0300

    Вынес код клиента

---
+++ internal/client/application.go
@@ -0,0 +1,26 @@
+package client
+
+import (
+	"fmt"
+	"github.com/dcwk/metrics/internal/config"
+	"github.com/dcwk/metrics/internal/handlers"
+	"time"
+)
+
+func Run(conf config.ClientConf) error {
+	var pollCount int64 = 0
+	fmt.Println("Sending metrics to", conf.ServerAddr)
+
+	for {
+		time.Sleep(time.Duration(conf.PollInterval) * time.Second)
+		if pollCount%conf.ReportInterval != 0 {
+			continue
+		}
+
+		if err := handlers.SendMetrics(conf.ServerAddr); err != nil {
+			return err
+		}
+
+		pollCount++
+	}
+}
