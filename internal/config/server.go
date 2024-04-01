ec399d0cab96dfbedafeb91abf924cef3117340f
Ruslan Golovizin <r.golovizin@webinar.ru>
2024-04-01 21:45:56 +0300

    Добавил в конфиг сервера новые флаги

--- internal/config/server.go
+++ internal/config/server.go
@@ -7,8 +7,11 @@ import (
 )
 
 type ServerConf struct {
-	ServerAddr string `env:"ADDRESS"`
-	LogLevel   string `env:"LOG_LEVEL"`
+	ServerAddr      string `env:"ADDRESS"`
+	LogLevel        string `env:"LOG_LEVEL"`
+	StoreInternal   int64  `env:"STORE_INTERNAL"`
+	FileStoragePath string `env:"FILE_STORAGE_PATH"`
+	Restore         bool   `env:"RESTORE"`
 }
 
 func NewServerConf() (*ServerConf, error) {
@@ -16,6 +19,9 @@ func NewServerConf() (*ServerConf, error) {
 
 	flag.StringVar(&conf.ServerAddr, "a", ":8080", "address and port to run server")
 	flag.StringVar(&conf.LogLevel, "l", "debug", "log level")
+	flag.Int64Var(&conf.StoreInternal, "i", 300, "store interval")
+	flag.StringVar(&conf.FileStoragePath, "f", "/tmp/metrics-db.json", "file storage path")
+	flag.BoolVar(&conf.Restore, "r", true, "load exist data at server start")
 	flag.Parse()
 
 	err := env.Parse(conf)
