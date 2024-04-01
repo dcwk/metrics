9d640df13dcbd85ed1da55df2bf10379b7a7bc35
Ruslan Golovizin <r.golovizin@webinar.ru>
2024-04-01 09:17:17 +0300

    Вернул отправку параметром как раньше

--- internal/handlers/getMetricByParams.go
+++ internal/handlers/getMetricByParams.go
@@ -1,37 +1,42 @@
 package handlers
 
 import (
-	"encoding/json"
+	"fmt"
 	"net/http"
 
-	"github.com/dcwk/metrics/internal/logger"
-	"github.com/dcwk/metrics/internal/models"
-	"github.com/dcwk/metrics/internal/service"
 	"github.com/go-chi/chi/v5"
 )
 
 func (h *Handlers) GetMetricByParams(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
-	metricsService := service.NewMetricsService(h.Storage)
 
 	t := chi.URLParam(r, "type")
-	mn := chi.URLParam(r, "name")
-	metrics := &models.Metrics{
-		ID:    mn,
-		MType: t,
-	}
+	n := chi.URLParam(r, "name")
+	v := ""
 
-	metrics, err := metricsService.GetMetrics(metrics)
-	if err != nil {
-		logger.Log.Error(err.Error())
-		http.Error(w, "", http.StatusInternalServerError)
+	switch t {
+	default:
+		http.Error(w, "", http.StatusBadRequest)
 		return
+	case gauge:
+		metricValue, err := h.Storage.GetGauge(n)
+		if err != nil {
+			http.Error(w, "", http.StatusNotFound)
+			return
+		}
+		v = fmt.Sprintf("%v", metricValue)
+	case counter:
+		metricValue, err := h.Storage.GetCounter(n)
+		if err != nil {
+			http.Error(w, "", http.StatusNotFound)
+			return
+		}
+		v = fmt.Sprintf("%d", metricValue)
 	}
 
 	w.WriteHeader(http.StatusOK)
-	if err := json.NewEncoder(w).Encode(metrics); err != nil {
-		logger.Log.Error(err.Error())
-		http.Error(w, err.Error(), http.StatusInternalServerError)
+	if _, err := w.Write([]byte(v)); err != nil {
+		http.Error(w, "", http.StatusInternalServerError)
 		return
 	}
 }
