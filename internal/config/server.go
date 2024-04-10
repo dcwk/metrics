c61841eedb4fcaa8040312186db39b4db85db39a
Ruslan Golovizin <r.golovizin@webinar.ru>
2024-04-10 17:12:08 +0300

    Изменил конфигурацию сервера

--- internal/config/server.go
+++ internal/config/server.go
@@ -18,10 +18,10 @@ func NewServerConf() (*ServerConf, error) {
 	conf := &ServerConf{}
 
 	flag.StringVar(&conf.ServerAddr, "a", "localhost:8080", "address and port to run server")
-	flag.StringVar(&conf.LogLevel, "l", "debug", "log level")
-	flag.Int64Var(&conf.StoreInterval, "i", 300, "store interval")
+	flag.StringVar(&conf.LogLevel, "l", "info", "log level")
+	flag.Int64Var(&conf.StoreInterval, "i", 10, "store interval")
 	flag.StringVar(&conf.FileStoragePath, "f", "/tmp/metrics-db.json", "file storage path")
-	flag.BoolVar(&conf.Restore, "r", false, "load exist data at server start")
+	flag.BoolVar(&conf.Restore, "r", true, "load exist data at server start")
 	flag.Parse()
 
 	err := env.Parse(conf)
