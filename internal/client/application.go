9a75da681393238421a68a17cbdb43c889658f41
Ruslan Golovizin <r.golovizin@webinar.ru>
2024-04-25 00:50:30 +0300

    Вынес отправку метрик из пакета handlers

--- internal/client/application.go
+++ internal/client/application.go
@@ -7,9 +7,7 @@ import (
 	"time"
 
 	"github.com/dcwk/metrics/internal/config"
-	"github.com/dcwk/metrics/internal/handlers"
 	"github.com/dcwk/metrics/internal/logger"
-	"github.com/dcwk/metrics/internal/storage"
 )
 
 func Run(ctx context.Context, conf *config.ClientConf) error {
@@ -35,17 +33,13 @@ func reportMetrics(ctx context.Context, wg *sync.WaitGroup, conf *config.ClientC
 	reportTicker := time.NewTicker(time.Duration(conf.ReportInterval) * time.Second)
 	defer reportTicker.Stop()
 
-	h := handlers.Handlers{
-		Storage: storage.NewStorage(),
-	}
-
 	for {
 		select {
 		case <-ctx.Done():
 			wg.Done()
 			return
 		case <-reportTicker.C:
-			_ = h.SendBatchMetrics(agent.Metrics, conf.ServerAddr, &agent.PollCount)
+			_ = SendBatchMetrics(agent.Metrics, conf.ServerAddr, &agent.PollCount)
 		}
 	}
 }
