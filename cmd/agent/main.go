1516c0c27f43b0d61a66554c80550d0ca6e97b2a
Ruslan Golovizin <dcwk@yandex.ru>
2024-07-03 20:21:12 +0300

    Добавил функцию для вывода buildInfo из глобальных переменных

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"os"
 	"os/signal"
@@ -11,11 +12,17 @@ import (
 	"github.com/dcwk/metrics/internal/config"
 )
 
+var buildVersion string
+var buildDate string
+var buildCommit string
+
 func main() {
 	conf, err := config.NewClientConf()
 	if err != nil {
 		log.Fatal(err)
 	}
+	buildInfo()
+
 	ctx, cancel := context.WithCancel(context.Background())
 	go func() {
 		stop := make(chan os.Signal, 1)
@@ -30,4 +37,20 @@ func main() {
 
 		return
 	}
+}
+
+func buildInfo() {
+	if buildVersion == "" {
+		buildVersion = "N/A"
+	}
+	if buildDate == "" {
+		buildDate = "N/A"
+	}
+	if buildCommit == "" {
+		buildCommit = "N/A"
+	}
+
+	fmt.Println(fmt.Sprintf("Build version: %s", buildVersion))
+	fmt.Println(fmt.Sprintf("Build date: %s", buildDate))
+	fmt.Println(fmt.Sprintf("Build commit: %s", buildCommit))
 }
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	_ "net/http/pprof"
 
@@ -8,11 +9,32 @@ import (
 	"github.com/dcwk/metrics/internal/server"
 )
 
+var buildVersion string
+var buildDate string
+var buildCommit string
+
 func main() {
 	conf, err := config.NewServerConf()
 	if err != nil {
 		log.Fatal(err)
 	}
 
+	buildInfo()
 	server.Run(conf)
+}
+
+func buildInfo() {
+	if buildVersion == "" {
+		buildVersion = "N/A"
+	}
+	if buildDate == "" {
+		buildDate = "N/A"
+	}
+	if buildCommit == "" {
+		buildCommit = "N/A"
+	}
+
+	fmt.Println(fmt.Sprintf("Build version: %s", buildVersion))
+	fmt.Println(fmt.Sprintf("Build date: %s", buildDate))
+	fmt.Println(fmt.Sprintf("Build commit: %s", buildCommit))
 }
