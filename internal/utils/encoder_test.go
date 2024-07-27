dc8bcf37a09ad3f84ea17f9de3c90a091531cf94
Ruslan Golovizin <dcwk@yandex.ru>
2024-07-27 22:36:08 +0300

    Добавил проверок в тест

--- internal/utils/encoder_test.go
+++ internal/utils/encoder_test.go
@@ -6,31 +6,75 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEncode(t *testing.T) {
+func TestEncoder(t *testing.T) {
 	tests := []struct {
-		Data     string
-		Expected string
+		PublicKeyPath   string
+		PrivateKeyPath  string
+		Data            string
+		ExpectedData    string
+		PublicPemError  bool
+		PrivatePemError bool
 	}{
 		{
-			Data:     "test",
-			Expected: "test",
+			PublicKeyPath:   "./keys/public.pem",
+			PrivateKeyPath:  "./keys/private.pem",
+			Data:            "test",
+			ExpectedData:    "test",
+			PublicPemError:  false,
+			PrivatePemError: false,
 		},
 		{
-			Data:     "",
-			Expected: "",
+			PublicKeyPath:   "./keys/public.pem",
+			PrivateKeyPath:  "./keys/private.pem",
+			Data:            "",
+			ExpectedData:    "",
+			PublicPemError:  false,
+			PrivatePemError: false,
 		},
 		{
-			Data:     "123123890###ololo",
-			Expected: "123123890###ololo",
+			PublicKeyPath:   "./keys/public.pem",
+			PrivateKeyPath:  "./keys/private.pem",
+			Data:            "123123890###ololo",
+			ExpectedData:    "123123890###ololo",
+			PublicPemError:  false,
+			PrivatePemError: false,
+		},
+		{
+			PublicKeyPath:   "./keys/oloo.pem",
+			PrivateKeyPath:  "./keys/private.pem",
+			Data:            "123123890###ololo",
+			ExpectedData:    "123123890###ololo",
+			PublicPemError:  true,
+			PrivatePemError: false,
+		},
+		{
+			PublicKeyPath:   "./keys/public.pem",
+			PrivateKeyPath:  "./keys/ololo.pem",
+			Data:            "123123890###ololo",
+			ExpectedData:    "123123890###ololo",
+			PublicPemError:  false,
+			PrivatePemError: true,
 		},
 	}
 
 	for _, test := range tests {
-		encodedData, err := Encrypt([]byte(test.Data), "./keys/public.pem")
-		assert.NoError(t, err)
+		encodedData, err := Encrypt([]byte(test.Data), test.PublicKeyPath)
+		if test.PublicPemError {
+			assert.Error(t, err)
+			assert.Nil(t, encodedData)
+			continue
+		} else {
+			assert.NoError(t, err)
+		}
 
-		decodedData, err := Decrypt(encodedData, "./keys/private.pem")
-		assert.NoError(t, err)
-		assert.Equal(t, test.Expected, string(decodedData))
+		decodedData, err := Decrypt(encodedData, test.PrivateKeyPath)
+		if test.PrivatePemError {
+			assert.Error(t, err)
+			assert.Nil(t, decodedData)
+			continue
+		} else {
+			assert.NoError(t, err)
+			assert.Equal(t, test.ExpectedData, string(decodedData))
+		}
 	}
 }
